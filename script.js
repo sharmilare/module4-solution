a50814fcac9c725bfd33a97ef2a8f0cbc709f979
sharmilareddy <152505485+sharmilare@users.noreply.github.com>
2023-12-14 20:23:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ names[8]="Priya";
 names[9]="Jim";
 for (var i = 0; i < names.length; i++) {
 if (names[i].chartAt(0) === 'J' || names[i].chartAt(0) === 'j') {
-    console.log("GoodBye"+names[i])
+    console.log("GoodBye "+ names[i])
 }
     else {
-        console.log("Hello"+names[i])
+        console.log("Hello "+ names[i])
    }
 }
 
