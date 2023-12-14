74e76984e6acd0f8048bdb06a0023ebad42a2c35
sharmilareddy <152505485+sharmilare@users.noreply.github.com>
2023-12-14 20:03:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function () {
-
 var names = ["Hima", "Sharmi", "Jyothi", "Manoj", "Pavan", "Frank", "Bhanu", "Krishna", "Priya", "Jim"];
 for (var i = 0; i < names.length; i++) {
-if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
-  }
+if (names[i].chartAt(0) == 'J' || names[i].chartAt(0) == 'j') {
+    console.log("GoodBye"+names[i])
+}
+    else {
+        console.log("Hello"+names[i])
+   }
 }
 
-})();
+
+
