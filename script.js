f202630ad6a4c45164e41c52155e62ab9a59b14d
sharmilareddy <152505485+sharmilare@users.noreply.github.com>
2023-12-14 20:40:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-var names = new Array();
-names[0]="Hima";
-names[1]="Sharmi";
-names[2]="jyothi";
-names[3]="Manoj";
-names[4]="Pavan";
-names[5]="Frank";
-names[6]="Bhanu";
-names[7]="Krishna";
-names[8]="Priya";
-names[9]="Jim";
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
 if (names[i].chartAt(0) === 'J' || names[i].chartAt(0) === 'j') {
     console.log("GoodBye "+ names[i])
