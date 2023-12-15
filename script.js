7b8e4f6ad92c2572044573ddca309040181a4ab5
sharmilareddy <152505485+sharmilare@users.noreply.github.com>
2023-12-15 19:22:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i = 0; i < names.length; i++) {
-    if (names[i].charAt(0).toLowerCase() === 'j') {
-        console.log("Goodbye " + names[i]);
+// Function to greet a name
+function greet(name) {
+    // Check if the name starts with 'J' or 'j'
+    if (name.charAt(0).toLowerCase() === 'j') {
+        return "Good Bye " + name;
     } else {
-        console.log("Hello " + names[i]);
+        return "Hello " + name;
     }
 }
+
+// Function to iterate through an array of names and print greetings
+function greetAll(namesArray) {
+    // Loop through each name in the array
+    for (var i = 0; i < namesArray.length; i++) {
+        // Print the greeting using the greet function
+        console.log(greet(namesArray[i]));
+    }
+}
+
+// Existing array of names
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+// Call the greetAll function with the names array
+greetAll(names);
