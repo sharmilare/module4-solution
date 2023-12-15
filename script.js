229806bb04ce0609be5c0e7fad887d38f9ad5d51
sharmilareddy <152505485+sharmilare@users.noreply.github.com>
2023-12-15 19:09:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-var names =["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
-         if (names[i].charAt(0).toLowerCase() === 'j') {
-    console.log("GoodBye "+ names[i]);
+    if (names[i].charAt(0).toLowerCase() === 'j') {
+        console.log("Goodbye " + names[i]);
+    } else {
+        console.log("Hello " + names[i]);
+    }
 }
-    else {
-        console.log("Hello "+ names[i]);
-   }
-}
-
-
-
